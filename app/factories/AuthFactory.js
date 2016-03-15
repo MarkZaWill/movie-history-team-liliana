faa748777233f60f85fd4b9efd6b7e3519771feb
Jeremy C. Swain <jeremycswain@icloud.com>
2016-03-15 16:33:11 -0500

    basic login and register functionality have been added.

---
+++ app/factories/AuthFactory.js
@@ -0,0 +1,39 @@
+"use strict";
+
+MovieApp.factory("authFactory", function () {
+
+  let ref = new Firebase("https://movie-history-proj.firebaseio.com/");
+
+  return {
+    /*
+      Determine if the client is authenticated
+     */
+    isAuthenticated () {
+      let authData = ref.getAuth();
+
+      if (authData) {
+        return true;
+      } else {
+        return false;
+      }
+    },
+    /*
+      Authenticate the client via Firebase
+     */
+    authenticate (credentials) {
+      return new Promise((resolve, reject) => {
+        ref.authWithPassword({
+          "email": credentials.email,
+          "password": credentials.password
+        }, (error, authData) => {
+          if (error) {
+            reject(error);
+          } else {
+            console.log("authWithPassword method completed successfully");
+            resolve(authData);
+          }
+        });
+      });
+    }
+  };
+});
