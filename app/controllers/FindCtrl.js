389b68b19a5e31c0c46c03b631af3784ccb148ec
Paloma Venegas Stephens <palomavenegasstephens@Palomas-MacBook-Pro.local>
2016-03-16 11:28:30 -0500

    adding search functionality

--- app/controllers/FindCtrl.js
+++ app/controllers/FindCtrl.js
@@ -10,22 +10,30 @@ MovieApp.controller("FindCtrl", [
   function ($scope, $http, $location, MovieFactory) {
     // Default property values for keys bound to input fields
     $scope.movies = [];
+    $scope.searchField = {
+      "title": ""
+    }
 
-    // Invoke the promise that reads from Firebase
-    MovieFactory().then(
-      function (moviesObj) {
-      // Handle resolve() from the promise
-        Object.keys(moviesObj).forEach(function (key) {
-          moviesObj[key] = key;
-          $scope.movies.push(moviesObj[key]);
-          console.log(key);
-        })
-      },
+    $scope.getMovies = function (movieTitle) {
+      $scope.apiCall = "http://www.omdbapi.com/?t=" +  movieTitle;
 
-      // Handle reject() from the promise
-      function (err) {
-        console.log(err)
-      }
-    )
+      // Invoke the promise that reads from Firebase
+      MovieFactory($scope.apiCall).then(
+        function (moviesObj) {
+        // Handle resolve() from the promise
+          Object.keys(moviesObj).forEach(function (key) {
+            moviesObj[key] = key;
+            $scope.movies.push(moviesObj[key]);
+            console.log(key);
+          })
+          console.log("maybe",moviesObj.Poster)
+        },
+
+        // Handle reject() from the promise
+        function (err) {
+          console.log(err)
+        }
+      )
+    }
   }
 ]);
